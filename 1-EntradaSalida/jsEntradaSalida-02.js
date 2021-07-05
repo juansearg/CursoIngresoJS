ee843a88109e7213a955e3a0ae7b0b68c9dfbaa6
juansearg <juansviazzo@gmail.com>
2021-07-04 21:18:12 -0300

    status add status commit push

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -2,7 +2,8 @@
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	alert("ok");
+{ 
+	let nombre = prompt	('Inserte dato: ');
+	alert(nombre);
 }
 
